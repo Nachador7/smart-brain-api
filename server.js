f47ad9f71cafbd49d25ef944b131d7d73d1175fc
Nachador7 <153395315+Nachador7@users.noreply.github.com>
2024-01-05 23:21:44 -0300

    changes

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+const e = require('express');
 const express = require('express');
+const bodyParser = require('body-parser');
+
 
 const app = express();
+
+app.use(bodyParser.json());
 
 const database = {
     users: [
@@ -28,9 +33,27 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-  
-    res.json('signing in');
+  if(req.body.email === database.users[0].email && 
+    req.body.password === database.users[0].password){
+    res.json('success');
+  } else {
+    res.status(400).json('error logging in');
+  }
 });
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date(),
+    })
+    res.json(database.users[database.users.length-1]);
+    })
+
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
 }
