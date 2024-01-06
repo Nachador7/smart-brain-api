0f863aca0c8fce2084d8bc2f6f4d7ab06b06c6dc
Nachador7 <153395315+Nachador7@users.noreply.github.com>
2024-01-06 20:58:05 -0300

    changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const e = require('express');
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const bcrypt = require('bcrypt-nodejs');
 
 const app = express();
 
@@ -25,11 +25,19 @@ const database = {
             entries: 0,
             joined: new Date(),
         }   
+    ],
+   login: [
+        {
+            id: '125',
+            hash: '',
+            email: 'john@gmail.com',
+        }
     ]
 }
 
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send(database.users);
 })
 
 app.post('/signin', (req, res) => {
@@ -43,6 +51,9 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
+    bcrypt.hash(password, null, null, function(err, hash) {
+      console.log(hash);
+    });
     database.users.push({
         id: '125',
         name: name,
@@ -52,7 +63,49 @@ app.post('/register', (req, res) => {
         joined: new Date(),
     })
     res.json(database.users[database.users.length-1]);
+    });
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if(user.id === id){
+            found = true;
+            return res.json(user);
+        }
     })
+    if(!found){
+        res.status(400).json('not found');
+    }
+});
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if(user.id === id){
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    })
+    if(!found){
+        res.status(400).json('not found');
+    }
+}
+);
+
+bcrypt.hash("bacon", null, null, function(err, hash) {
+ 
+});
+
+
+// bcrypt.compare("bacon", hash, function(err, res) {
+  
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+  
+// });
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
