2080cda3c328c71d79a4b5d84799a29afcd74fb0
Nachador7 <153395315+Nachador7@users.noreply.github.com>
2024-01-07 21:14:04 -0300

    changes

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex')
-const fetch = require('node-fetch');
 
 const register = require('./Controlers/register');
 const signin = require('./Controlers/signin');
@@ -35,44 +34,6 @@ app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)});
 app.post('/register',(req, res) => { register.handleRegister(req, res, db, bcrypt)});
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
 app.put('/image', (req, res) => {image.handleImage(req, res, db)});
-
-
-app.post('/clarifai', async (req, res) => {
-  try {
-    const { imageUrl } = req.body;
-
-    const clarifaiResponse = await fetch('https://api.clarifai.com/v2/models/face-detection/outputs', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Key your_clarifai_api_key', // Replace with your actual Clarifai API key
-      },
-      body: JSON.stringify({
-        "user_app_id": {
-          "user_id": "nachador",
-          "app_id": "test",
-        },
-        "inputs": [
-          {
-            "data": {
-              "image": {
-                "url": imageUrl,
-              }
-            }
-          }
-        ]
-      }),
-    });
-
-    const clarifaiData = await clarifaiResponse.json();
-    res.json(clarifaiData);
-  } catch (error) {
-    console.error('Clarifai API call failed', error);
-    res.status(400).json('Clarifai API call failed');
-  }
-});
-
-
 
 const PORT = process.env.PORT;
 
